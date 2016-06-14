0800bd1eddecfb9d9a9728f993a4d37b6d75641c
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-06-15 00:28:47 +0530

    Add files via upload

---
+++ give.js
@@ -0,0 +1,350 @@
+function givepicsound() {
+    var c = document.getElementById("t1").value;
+    if (c.toLowerCase() == 'a') {
+        document.getElementById("im").innerHTML = '<img src="a.jpe">';
+        document.getElementById("ao").innerHTML = "A For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Apple" + '<br>' + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">A For Apple!! Must have One.</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'b') {
+        document.getElementById("im").innerHTML = '<img src="b.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="b.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "B For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Ball" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">B For Ball! Time to Play.</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'c') {
+        document.getElementById("im").innerHTML = '<img src="c.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="c.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "C For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Cup of Caffeine" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="alternate">Convert Caffeine to <"Code"></marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'd') {
+        document.getElementById("im").innerHTML = '<img src="d.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="d.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "D For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Donuts" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Party Time!!</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'e') {
+        document.getElementById("im").innerHTML = '<img src="e.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="e.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "E For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Elephant" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Waana Have a ride?</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'f') {
+        document.getElementById("im").innerHTML = '<img src="f.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="f.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "F For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Fan" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">A Life Saver!</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'g') {
+        document.getElementById("im").innerHTML = '<img src="g.png">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="g.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "G For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Google" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Just GOOGLE it</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'h') {
+        document.getElementById("im").innerHTML = '<img src="h.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="h.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "H For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Horse" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Can you Run faster?</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'i') {
+        document.getElementById("im").innerHTML = '<img src="i.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="i.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "I For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "iPhone" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Go for the latest.</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'j') {
+        document.getElementById("im").innerHTML = '<img src="j.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="j.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "J For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Jelly" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Mouth Watering!</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'k') {
+        document.getElementById("im").innerHTML = '<img src="k.png">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="k.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "K For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Kite" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Fly your dreams high.</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'l') {
+        document.getElementById("im").innerHTML = '<img src="l.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="l.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "L For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Lion" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">An inspiration of strength.</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'm') {
+        document.getElementById("im").innerHTML = '<img src="m.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="m.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "M For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Money" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Worth to Earn!</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'n') {
+        document.getElementById("im").innerHTML = '<img src="n.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="n.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "N For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Nest" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Protected from the world outside!</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'o') {
+        document.getElementById("im").innerHTML = '<img src="o.png">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="o.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "O For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Octopus" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Ever met this Mr ?</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'p') {
+        document.getElementById("im").innerHTML = '<img src="p.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="p.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "P For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Parrot" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Speak to get imitated!</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'q') {
+        document.getElementById("im").innerHTML = '<img src="q.png">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="q.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "Q For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Question Mark" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Think it over</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'r') {
+        document.getElementById("im").innerHTML = '<img src="r.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="r.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "R For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Rabbit" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Nice cutties</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 's') {
+        document.getElementById("im").innerHTML = '<img src="s.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="s.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "S For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Sun" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Ultimate source of energy</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 't') {
+        document.getElementById("im").innerHTML = '<img src="t.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="t.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "T For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Television(TV)" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Where you watch your shows</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'u') {
+        document.getElementById("im").innerHTML = '<img src="u.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="u.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "U For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Umbrella" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Have it during rains</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'v') {
+        document.getElementById("im").innerHTML = '<img src="v.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="v.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "V For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Van" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Used to pick and drop to school</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'w') {
+        document.getElementById("im").innerHTML = '<img src="w.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="w.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "W For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Wikipedia" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Search form here anywhere</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'x') {
+        document.getElementById("im").innerHTML = '<img src="x.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="x.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "X For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "X-MAS" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Merry X-MAS</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'y') {
+        document.getElementById("im").innerHTML = '<img src="y.jpe">';
+        // document.getElementById("ao").innerHTML='<audio controls="controls"><source src="y.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "Y For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Yoga" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Do Yoga Everyday</marquee>'
+        }
+    }
+    else if (c.toLowerCase() == 'z') {
+        document.getElementById("im").innerHTML = '<img src="z.jpe">';
+        //document.getElementById("ao").innerHTML='<audio controls="controls"><source src="z.mp3"></audio>';
+        document.getElementById("ao").innerHTML = "Z For";
+        window.setTimeout(gs, 1500);
+        function gs() {
+            document.getElementById("ao").innerHTML = "Zebra" + '<br>';
+        }
+        setTimeout(ms, 2000);
+        function ms() {
+            document.getElementById("ao").innerHTML += '<marquee behaviour="scroll" direction="left">Zzzzz Go!</marquee>'
+        }
+    }
+    else {
+        document.getElementById("ao").innerHTML = '<marquee behaviour="scroll" direction="left">Enter a Valid Character(ABC...XYZ)</marquee>'
+        document.getElementById("im").innerHTML="";
+    }
+}
+function del() {
+    document.getElementById("t1").value = "";
+}
+function cap() {
+    document.getElementById("t1").value = document.getElementById("t1").value.toUpperCase();
+}
